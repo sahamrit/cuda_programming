69b7ef25c08629cbd5226aa1dcb1ca0d5e78c63c
sahamrit <amritsahu.iitkgp@gmail.com>
2023-03-07 23:56:24 +0530

    Add vector addition with initialisations in kernel

---
+++ page_fault.cu
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<assert.h>
+
+__global__
+void deviceKernel(int *a, int N)
+{
+  int idx = threadIdx.x + blockIdx.x * blockDim.x;
+  int stride = blockDim.x * gridDim.x;
+
+  for (int i = idx; i < N; i += stride)
+  {
+    a[i] = 1;
+  }
+}
+
+void hostFunction(int *a, int N)
+{
+  for (int i = 0; i < N; ++i)
+  {
+    a[i] = 1;
+  }
+}
+
+inline cudaError_t checkCuda(cudaError_t result)
+{
+  if (result != cudaSuccess) {
+    fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
+    assert(result == cudaSuccess);
+  }
+  return result;
+}
+
+int main()
+{
+
+  int N = 2<<24;
+  size_t size = N * sizeof(int);
+  int *a;
+  cudaMallocManaged(&a, size);
+
+
+// Only CPU
+   hostFunction(a, N);
+
+// Only GPU
+   deviceKernel<<<1,1>>>(a,N);
+   checkCuda( cudaGetLastError());
+   checkCuda( cudaDeviceSynchronize());
+
+   
+  /*
+   * Conduct experiments to learn more about the behavior of
+   * `cudaMallocManaged`.
+   *
+   * What happens when unified memory is accessed only by the GPU?
+   * What happens when unified memory is accessed only by the CPU?
+   * What happens when unified memory is accessed first by the GPU then the CPU?
+   * What happens when unified memory is accessed first by the CPU then the GPU?
+   *
+   * Hypothesize about UM behavior, page faulting specificially, before each
+   * experiment, and then verify by running `nsys`.
+   */
+
+
+// When the Unified Memory is accessed from the GPU or CPU only no page fault, DtoH, HtoD transfer occurs
+  cudaFree(a);
+}
---
+++ vector_addition_optimized_init_in_kernel.cu
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * Host function to initialize vector elements. This function
+ * simply initializes each element to equal its index in the
+ * vector.
+ */
+
+__global__ 
+void initWith(float num, float *a, int N)
+{
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+    int stride = blockDim.x * gridDim.x;
+  
+    for(int i = index; i < N; i += stride)
+    {
+      a[i] = num;
+    }
+}
+
+/*
+ * Device kernel stores into `result` the sum of each
+ * same-indexed value of `a` and `b`.
+ */
+
+__global__
+void addVectorsInto(float *result, float *a, float *b, int N)
+{
+  int index = threadIdx.x + blockIdx.x * blockDim.x;
+  int stride = blockDim.x * gridDim.x;
+
+  for(int i = index; i < N; i += stride)
+  {
+    result[i] = a[i] + b[i];
+  }
+}
+
+/*
+ * Host function to confirm values in `vector`. This function
+ * assumes all values are the same `target` value.
+ */
+
+void checkElementsAre(float target, float *vector, int N)
+{
+  for(int i = 0; i < N; i++)
+  {
+    if(vector[i] != target)
+    {
+      printf("FAIL: vector[%d] - %0.0f does not equal %0.0f\n", i, vector[i], target);
+      exit(1);
+    }
+  }
+  printf("Success! All values calculated correctly.\n");
+}
+
+pair <int,int> getGridConfig()
+{
+
+  /*
+   * nsys should register performance changes when execution configuration
+   * is updated.
+   */
+   int deviceId;
+   cudaGetDevice(&deviceId); 
+    
+   cudaDeviceProp props; 
+   cudaGetDeviceProperties(&props, deviceId);
+ 
+   int multiProcessorCount, maxThreadsPerBlock;
+   maxThreadsPerBlock = props.maxThreadsPerBlock;   
+   multiProcessorCount = props.multiProcessorCount; 
+ 
+   printf("multiProcessorCount: %d maxThreadsPerBlock: %d\n",multiProcessorCount,maxThreadsPerBlock);
+
+   return make_pair(maxThreadsPerBlock, multiProcessorCount);
+}
+
+int main()
+{
+  const int N = 2<<24;
+  size_t size = N * sizeof(float);
+  
+  int size_float = sizeof(float);
+
+  printf("Size of Float: %d\n", size_float);
+
+  int threadsPerBlock;
+  int numberOfBlocks;
+  
+  pair<int,int> gridConfig = getGridConfig();
+
+  threadsPerBlock = gridConfig.first;
+  numberOfBlocks = gridConfig.second;
+  printf("numberOfBlocks: %d threadsPerBlock: %d\n",numberOfBlocks,threadsPerBlock);
+
+  float *a;
+  float *b;
+  float *c;
+
+  cudaMallocManaged(&a, size);
+  cudaMallocManaged(&b, size);
+  cudaMallocManaged(&c, size);
+
+  initWith<<<numberOfBlocks, threadsPerBlock>>>(3, a, N);
+  initWith<<<numberOfBlocks, threadsPerBlock>>>(4, b, N);
+  initWith<<<numberOfBlocks, threadsPerBlock>>>(0, c, N);
+
+  addVectorsInto<<<numberOfBlocks, threadsPerBlock>>>(c, a, b, N);
+
+  cudaError_t addVectorsErr;
+  cudaError_t asyncErr;
+
+  addVectorsErr = cudaGetLastError();
+  if(addVectorsErr != cudaSuccess) printf("Error: %s\n", cudaGetErrorString(addVectorsErr));
+
+  asyncErr = cudaDeviceSynchronize();
+  if(asyncErr != cudaSuccess) printf("Error: %s\n", cudaGetErrorString(asyncErr));
+
+  checkElementsAre(7, c, N);
+
+  cudaFree(a);
+  cudaFree(b);
+  cudaFree(c);
+}
