7068779d169f00c608863ba82715684eb62b21d8
sahamrit <amritsahu.iitkgp@gmail.com>
2023-03-09 15:47:14 +0000

    Add concurrent init vector addition with prefetch

---
+++ lab_03_cuda_streams/vector_addition_concurrent_init.cu
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+__global__
+void initWith(float num, float *a, int N)
+{
+
+  int index = threadIdx.x + blockIdx.x * blockDim.x;
+  int stride = blockDim.x * gridDim.x;
+
+  for(int i = index; i < N; i += stride)
+  {
+    a[i] = num;
+  }
+}
+
+__global__
+void addVectorsInto(float *result, float *a, float *b, int N)
+{
+  int index = threadIdx.x + blockIdx.x * blockDim.x;
+  int stride = blockDim.x * gridDim.x;
+
+  for(int i = index; i < N; i += stride)
+  {
+    result[i] = a[i] + b[i];
+  }
+}
+
+void checkElementsAre(float target, float *vector, int N)
+{
+  for(int i = 0; i < N; i++)
+  {
+    if(vector[i] != target)
+    {
+      printf("FAIL: vector[%d] - %0.0f does not equal %0.0f\n", i, vector[i], target);
+      exit(1);
+    }
+  }
+  printf("Success! All values calculated correctly.\n");
+}
+
+int main()
+{
+  int deviceId;
+  int numberOfSMs;
+
+  cudaGetDevice(&deviceId);
+  cudaDeviceGetAttribute(&numberOfSMs, cudaDevAttrMultiProcessorCount, deviceId);
+
+  const int N = 2<<24;
+  size_t size = N * sizeof(float);
+
+  float *a;
+  float *b;
+  float *c;
+
+  cudaMallocManaged(&a, size);
+  cudaMallocManaged(&b, size);
+  cudaMallocManaged(&c, size);
+
+  cudaMemPrefetchAsync(a, size, deviceId);
+  cudaMemPrefetchAsync(b, size, deviceId);
+  cudaMemPrefetchAsync(c, size, deviceId);
+
+  size_t threadsPerBlock;
+  size_t numberOfBlocks;
+
+  threadsPerBlock = 256;
+  numberOfBlocks = 32 * numberOfSMs;
+
+  cudaError_t addVectorsErr;
+  cudaError_t asyncErr;
+
+  cudaStream_t stream;
+  cudaStreamCreate(&stream);
+  initWith<<<numberOfBlocks, threadsPerBlock, 0, stream>>>(3, a, N);
+  cudaStreamDestroy(stream);
+
+  cudaStreamCreate(&stream);
+  initWith<<<numberOfBlocks, threadsPerBlock, 0, stream>>>(4, b, N);
+  cudaStreamDestroy(stream);
+
+  cudaStreamCreate(&stream);
+  initWith<<<numberOfBlocks, threadsPerBlock, 0, stream>>>(0, c, N);
+  cudaStreamDestroy(stream);
+
+// We don't need to explicitly synchronise because it will execute in default stream which is blocking
+  addVectorsInto<<<numberOfBlocks, threadsPerBlock>>>(c, a, b, N);
+
+  addVectorsErr = cudaGetLastError();
+  if(addVectorsErr != cudaSuccess) printf("Error: %s\n", cudaGetErrorString(addVectorsErr));
+
+  asyncErr = cudaDeviceSynchronize();
+  if(asyncErr != cudaSuccess) printf("Error: %s\n", cudaGetErrorString(asyncErr));
+
+  cudaMemPrefetchAsync(c, size, cudaCpuDeviceId);
+
+  checkElementsAre(7, c, N);
+
+  cudaFree(a);
+  cudaFree(b);
+  cudaFree(c);
+}
